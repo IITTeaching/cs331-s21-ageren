0b62e77868918d4f1c52f61279cbab49b838ad0b
allygeren <allygeren2022@gmail.com>
2021-05-12 22:12:47 -0500

    Finished lab11

--- lab11/lab11.py
+++ lab11/lab11.py
@@ -4,20 +4,55 @@ import random
 def quicksort(lst,pivot_fn):
     qsort(lst,0,len(lst) - 1,pivot_fn)
 
+def hoare_scheme(lst, low, high, pivot_fn):
+    pivot = pivot_fn(lst, low, high)
+    i = low
+    j = high
+    while i<j:
+        while lst[i]<pivot:
+            i += 1
+        while lst[j]>pivot:
+            j -= 1
+        if i>=j:
+            return [lst,j]
+        else:
+            lst[i], lst[j] = lst[j], lst[i]
+
 def qsort(lst,low,high,pivot_fn):
     ### BEGIN SOLUTION
+    if high <= low+1:
+        if low<=high and lst[high]<lst[low] and low>-1 and high<len(lst):
+            lst[low], lst[high] = lst[high], lst[low]
+        return lst
+    else:
+        part = hoare_scheme(lst,low,high,pivot_fn)
+        lst = part[0]
+        lst = qsort(lst,low,part[1],pivot_fn)
+        lst = qsort(lst,part[1]+1,high,pivot_fn)
+    return lst
     ### END SOLUTION
 
 def pivot_first(lst,low,high):
     ### BEGIN SOLUTION
+    return low
     ### END SOLUTION
 
 def pivot_random(lst,low,high):
     ### BEGIN SOLUTION
+    return random.randint(low, high-1)
     ### END SOLUTION
 
 def pivot_median_of_three(lst,low,high):
     ### BEGIN SOLUTION
+    lowest = lst[low]
+    middle = lst[(low+high)//2]
+    highest = lst[high]
+    if (lowest>=middle and lowest<=highest) or (lowest<=middle and lowest>=highest):
+        return low
+    elif (middle>=lowest and middle<=highest) or (middle<=lowest and middle>=highest):
+        return (low+high)//2
+    else:
+        return high
     ### END SOLUTION
 
 ################################################################################
